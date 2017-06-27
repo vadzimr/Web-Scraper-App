6f10ad1cc0aec2223603e90145a1304d72e1643b
VADIMRAYINCHICK <VADIM.RAYINCHICK@GMAIL.COM>
2017-06-27 14:08:15 -0700

    final commit

---
+++ scripts/scrape.js
@@ -0,0 +1,52 @@
+
+
+// Require request and cheerio, making our scrapes possible
+var request = require("request");
+var cheerio = require("cheerio");
+
+// This function will scrape the NYTimes website (cb is our callback)
+var scrape = function(cb) {
+
+  // Use the request package to take in the body of the page's html
+  request("http://www.nytimes.com", function(err, res, body) {
+    // body is the actual HTML on the page. Load this into cheerio
+
+    var $ = cheerio.load(body);
+
+    // Make an empty array to save our article info
+    var articles = [];
+
+    // Now, find and loop through each element that has the "theme-summary" class
+    // (i.e, the section holding the articles)
+    $(".theme-summary").each(function(i, element) {
+
+      // Then we grab the inner text of the this element and store it
+      // to the head variable. This is the article headline
+      var head = $(this).children(".story-heading").text().trim();
+
+
+      // We store this to the sum variable. This is the article summary
+      var sum = $(this).children(".summary").text().trim();
+
+      // So long as our headline and sum aren't empty or undefined, do the following
+      if (head && sum) {
+
+        
+
+        // Initialize an object we will push to the articles array
+
+        var dataToAdd = {
+          headline: headNeat,
+          summary: sumNeat
+        };
+
+        articles.push(dataToAdd);
+      }
+    });
+    // After our loop is complete, send back the array of articles to the callback function
+    cb(articles);
+  });
+};
+
+// Export the function, so other files in our backend can use it
+module.exports = scrape;
